a5fcb59a8d4fe60ec20529860e48915a8e645bf9
Luis Vegerano <lvegerano422@gmail.com>
2020-01-16 10:28:10 -0600

    [CHORE] Rename blueprint

--- blueprints/ember-sortablejs/index.js
+++ blueprints/ember-sortablejs/index.js
@@ -3,6 +3,12 @@
 module.exports = {
   description: 'adds sortablejs to application',
 
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
+
   afterInstall() {
     return this.addPackageToProject('sortablejs', '^1.10.x')
   }
